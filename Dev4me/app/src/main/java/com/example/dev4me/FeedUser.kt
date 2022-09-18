4fa65d37315295bdbf6a4fbf6445edab1e4e13d7
gui0103 <gui03costa>
2022-09-17 23:01:23 -0300

    updating activities

---
+++ Dev4me/app/src/main/java/com/example/dev4me/FeedUser.kt
@@ -0,0 +1,36 @@
+package com.example.dev4me
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FeedUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+
+//    BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//        when(item.itemId) {
+//            R.id.item1 -> {
+//                // Respond to navigation item 1 click
+//                true
+//            }
+//            R.id.item2 -> {
+//                // Respond to navigation item 2 click
+//                true
+//            }
+//            else -> false
+//        }
+//    }
+//
+//    bottomNavigation.setOnNavigationItemReselectedListener { item ->
+//        when(item.itemId) {
+//            R.id.item1 -> {
+//                // Respond to navigation item 1 reselection
+//            }
+//            R.id.item2 -> {
+//                // Respond to navigation item 2 reselection
+//            }
+//        }
+//    }
+}
