b30a3f84ada7cce2d51a3e25b06bef1721ab5908
gui0103 <gui03costa@gmail.com>
2022-10-01 21:15:00 -0300

    starting navigation (feedUser navigation)

--- Dev4me/app/src/main/java/com/example/dev4me/FeedUser.kt
+++ Dev4me/app/src/main/java/com/example/dev4me/FeedUser.kt
@@ -1,11 +1,22 @@
 package com.example.dev4me
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dev4me.databinding.ActivityFeedUserBinding
 
 class FeedUser : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFeedUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_feed_user)
+        binding = ActivityFeedUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.feedUserCard1.setOnClickListener {
+            val navigateToOpenedCard = Intent(this, OpenedCardJob::class.java)
+            startActivity(navigateToOpenedCard)
+        }
     }
 }
--- Dev4me/app/src/main/java/com/example/dev4me/OpenedCardJob.kt
+++ Dev4me/app/src/main/java/com/example/dev4me/OpenedCardJob.kt
@@ -3,7 +3,7 @@ package com.example.dev4me
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class openedCardJob : AppCompatActivity() {
+class OpenedCardJob : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_opened_card_job)
