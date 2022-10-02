e7b3a7ec6fb9c4282c86904352fdf9ee3db9dd0a
gui0103 <gui03costa@gmail.com>
2022-10-01 21:28:38 -0300

    login to feedUser navigation

--- Dev4me/app/src/main/java/com/example/dev4me/MainActivity.kt
+++ Dev4me/app/src/main/java/com/example/dev4me/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.dev4me
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dev4me.databinding.ActivityFeedUserBinding
+import com.example.dev4me.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.entrarBtn.setOnClickListener {
+            val navigateToFeedUser = Intent(this, FeedUser::class.java)
+            startActivity(navigateToFeedUser)
+        }
     }
 }
