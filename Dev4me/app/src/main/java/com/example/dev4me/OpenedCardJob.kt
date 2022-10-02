4a36622279a3f7cdbd9237106acd15b8bb3fa067
gui0103 <gui03costa@gmail.com>
2022-10-01 23:12:35 -0300

    Alert Dialog for job applies built

--- Dev4me/app/src/main/java/com/example/dev4me/OpenedCardJob.kt
+++ Dev4me/app/src/main/java/com/example/dev4me/OpenedCardJob.kt
@@ -2,10 +2,30 @@ package com.example.dev4me
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dev4me.databinding.ActivityOpenedCardJobBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class OpenedCardJob : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOpenedCardJobBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_opened_card_job)
+        binding = ActivityOpenedCardJobBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.applyButton.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle(resources.getString(R.string.alert_title))
+                .setMessage(resources.getString(R.string.alert_message))
+                .setNegativeButton(resources.getString(R.string.alert_no)) { dialog, which ->
+                    // Do nothing
+                }
+                .setPositiveButton(resources.getString(R.string.alert_yes)) { dialog, which ->
+                    // Notify company and add to user candidacies
+                    // Disable "apply now" button
+                }
+                .show()
+        }
     }
 }
