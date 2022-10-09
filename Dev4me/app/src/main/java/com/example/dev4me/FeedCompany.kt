27d73da045e07edb2875a17e74511d004312bce9
gui0103 <gui03costa@gmail.com>
2022-10-09 16:22:44 -0300

    feed company navigation

--- Dev4me/app/src/main/java/com/example/dev4me/FeedCompany.kt
+++ Dev4me/app/src/main/java/com/example/dev4me/FeedCompany.kt
@@ -1,11 +1,22 @@
 package com.example.dev4me
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dev4me.databinding.ActivityFeedCompanyBinding
 
 class FeedCompany : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFeedCompanyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_feed_company)
+        binding = ActivityFeedCompanyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.personCard1.setOnClickListener {
+            val navigatePersonProfile = Intent(this, PersonProfileView::class.java)
+            startActivity(navigatePersonProfile)
+        }
     }
 }
