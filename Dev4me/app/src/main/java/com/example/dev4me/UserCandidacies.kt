3f588e7cbb09e25d0198a17d20fb7e6a24bd8d8b
gui0103 <gui03costa@gmail.com>
2022-10-09 19:25:44 -0300

    user candidacies done + navigation

--- Dev4me/app/src/main/java/com/example/dev4me/UserCandidacies.kt
+++ Dev4me/app/src/main/java/com/example/dev4me/UserCandidacies.kt
@@ -1,27 +1,22 @@
 package com.example.dev4me
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dev4me.databinding.ActivityUserCandidaciesBinding
 
 class UserCandidacies : AppCompatActivity() {
 
-private lateinit var binding: ActivityUserCandidaciesBinding
+    private lateinit var binding: ActivityUserCandidaciesBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityUserCandidaciesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-     binding = ActivityUserCandidaciesBinding.inflate(layoutInflater)
-     setContentView(binding.root)
-
-        setSupportActionBar(findViewById(R.id.toolbar))
-        binding.toolbarLayout.title = title
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        binding.candidacyCard1.setOnClickListener {
+            val navigateToOpenedCard = Intent(this, OpenedCardJob::class.java)
+            startActivity(navigateToOpenedCard)
         }
     }
 }
