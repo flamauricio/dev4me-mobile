77242d26359680a42d5254c4fd988b8463448934
unknown <brenodfreitas7@gmail.com>
2022-10-27 19:59:04 -0300

    implementing the change password feature

--- Dev4me/app/src/main/java/com/example/dev4me/endpoints/Usuario.kt
+++ Dev4me/app/src/main/java/com/example/dev4me/endpoints/Usuario.kt
@@ -3,9 +3,13 @@ package com.example.dev4me.endpoints
 import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.Path
 
 interface Usuario {
     @GET("/usuarios/perfil/{id}")
     fun getUsuario(@Path("id") id: Int): Call<JsonObject>
+
+    @PATCH("/usuarios/senha/{id}")
+    fun patchSenhaUsuario(@Path("id") id: Int): Call<Unit>
 }
