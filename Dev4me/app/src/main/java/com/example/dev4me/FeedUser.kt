9cf67ca9f67531506135d8e3cd01bc6a4bbe047e
gui0103 <guilherme.ccosta@bandtec.com.br>
2022-12-09 20:24:20 -0300

    recycler feed user finally working

--- Dev4me/app/src/main/java/com/example/dev4me/FeedUser.kt
+++ Dev4me/app/src/main/java/com/example/dev4me/FeedUser.kt
@@ -4,39 +4,28 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.example.dev4me.adapter.JobsAdapter
 import com.example.dev4me.databinding.ActivityFeedUserBinding
+import com.example.dev4me.dto.JobRequest
 import com.example.dev4me.endpoints.Vaga
 import com.example.dev4me.retrofit.Rest
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class FeedUser : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
     private lateinit var binding: ActivityFeedUserBinding
 
     val retrofit = Rest.getInstance()
-    var jobsList: List<JsonObject> = listOf()
+    var jobsList: List<JobRequest> = listOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFeedUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
         bringJobs()
-
-        recyclerView = binding.recyclerViewFeedUser
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.setHasFixedSize(true)
-        recyclerView.adapter = AdapterJobs(jobsList)
-
-//        binding.feedUserCard1.setOnClickListener {
-//            val navigateToOpenedCard = Intent(this, OpenedCardJob::class.java)
-//            startActivity(navigateToOpenedCard)
-//        }
 
         binding.profileIcon.setOnClickListener {
             val navigateToProfile = Intent(this, UserMenu::class.java)
@@ -51,17 +40,23 @@ class FeedUser : AppCompatActivity() {
         )
 
         jobRequest.getVagas().enqueue(
-            object : Callback<List<JsonObject>> {
+            object : Callback<List<JobRequest>> {
                 override fun onResponse(
-                    call: Call<List<JsonObject>>,
-                    response: Response<List<JsonObject>>
+                    call: Call<List<JobRequest>>,
+                    response: Response<List<JobRequest>>
                 ) {
                     if (response.code() == 200) {
                         jobsList = response.body()!!
+                        MaterialAlertDialogBuilder(this@FeedUser)
+                            .setMessage(jobsList.toString())
+                            .show()
+                        val layoutManager = LinearLayoutManager(this@FeedUser)
+                        binding.recyclerViewFeedUser.layoutManager = layoutManager
+                        val adapter = JobsAdapter(jobsList)
+                        binding.recyclerViewFeedUser.adapter = adapter
                     }
                 }
-
-                override fun onFailure(call: Call<List<JsonObject>>, t: Throwable) {
+                override fun onFailure(call: Call<List<JobRequest>>, t: Throwable) {
                     MaterialAlertDialogBuilder(this@FeedUser)
                         .setMessage(resources.getString(R.string.api_error))
                         .show()
